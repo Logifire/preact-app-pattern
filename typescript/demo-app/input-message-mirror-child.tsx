907b31d03f33b6d9298f296c64293f85471c9655
Logifire <Logifire@users.noreply.github.com>
2021-03-22 19:55:11 +0100

    Added doc-block to property

--- typescript/demo-app/input-message-mirror-child.tsx
+++ typescript/demo-app/input-message-mirror-child.tsx
@@ -2,6 +2,9 @@ import { Component, h } from "preact";
 
 export class InputMessageMirrorChild extends Component<unknown, unknown> {
 
+    /**
+     * This is only to visualize preacts rendering mechanism and avoid dispatching a render event. Do not do this.
+     */
     private static countUpdate = 0;
 
     render() {
